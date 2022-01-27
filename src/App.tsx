16f9c2eab78b025347531a6d77a8505f828ea6d5
Yauheni Kadyrau <7077634@gmail.com>
2022-01-27 14:17:05 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Ratting} from "./components/Ratting/Ratting";
+
+function App() {
+    console.log('App rendering')
+    return (
+        <div>
+            <PageTitle title={'This APP component'}/>
+            <PageTitle title={'My friends'}/>
+            Article1
+            <Ratting value={2}/>
+            <Accordion titleValue={'Menu'} collapsed={true}/>
+            <Accordion titleValue={'Contact'} collapsed={true}/>
+            Article2
+            <Ratting value={5}/>
+            <Ratting value={0}/>
+            <Ratting value={1}/>
+            <Ratting value={5}/>
+
+        </div>)
+}
+type PageTitlePropsType={
+    title: string
+}
+function PageTitle(props:PageTitlePropsType) {
+    console.log('PageTitle rendering')
+    return (<h1>{props.title}</h1>)
+}
+
+
+export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+type AccordionPropsType={
+    titleValue: string
+    collapsed:boolean
+}
+const Accordion = function (props:AccordionPropsType) {
+
+    console.log('Accordion rendering')
+   if(props.collapsed) {return (<div>
+            <AccordionTitle title={props.titleValue}/>
+
+        </div>
+    )} else {
+    return (<div>
+            <AccordionTitle title={props.titleValue}/>
+            <AccordionBody/>
+        </div>
+    )}
+}
+type AccordionTitlePropsType={
+    title: string
+}
+function AccordionTitle(props:any) {
+    console.log('AccordionTitle rendering')
+    return (
+        <div>{props.title}</div>
+    )
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+
+    )
+}
+export default Accordion;
